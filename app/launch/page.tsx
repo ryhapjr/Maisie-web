ed020189eb51baf74870a436c66411e7ee4189e4
Jojitoon <jojitoon@gmail.com>
2025-06-18 16:44:33 +0100

    fix: permissiions

--- app/launch/page.tsx
+++ app/launch/page.tsx
@@ -20,8 +20,12 @@ const LaunchPage = () => {
   const scopes = [
     'openid',
     'fhirUser',
+    'profile',
     'patient/Patient.read',
     'patient/Observation.read',
+    'patient/Condition.read',
+    'patient/AllergyIntolerance.read',
+    'patient/CarePlan.read',
     // 'launch/patient',
     'launch',
   ]; // Define required scopes
