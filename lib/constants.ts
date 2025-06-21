bf2a1a1f807ec5089e9abf2d0bc0bf0afabb6f1c
Jojitoon <jojitoon@gmail.com>
2025-06-21 23:51:21 +0100

    fix: crud

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,12 @@ export const SCOPES = [
   'openid',
   'fhirUser',
   'profile',
-  'patient/*.*',
+  'patient/Patient.read',
+  'patient/Observation.read',
+  'patient/Condition.read',
+  'patient/AllergyIntolerance.read',
+  'patient/CarePlan.read',
+  'patient/CarePlan.write',
   'launch/patient',
   'launch',
 ]; // Define required scopes
